50e3d75444c1413eab841107e06bd14a4ab462e7
[yubin-im] <[ybyb130@gmail.com]>
2023-08-10 00:10:54 +0900

    20230809

---
+++ 20230809/d230808/Pokemon.java
@@ -0,0 +1,22 @@
+package d230808;
+
+import java.util.Map;
+
+public class Pokemon {
+	// 포켓몬 닉네임(문자열), 포켓몬 현재체력(정수), 포켓몬 스텟(맵), 포켓몬 스킬(맵) 필드 구성
+	String pkNname;
+	int power;
+	Map<String, Object> statMap;
+	Map<String, Integer> skillMap;
+	
+	// Pokemon 생성자
+	public Pokemon() {
+		
+	}
+
+	
+	// 유즈케이스에 맞게 이후 메소드 구성
+
+
+
+}
